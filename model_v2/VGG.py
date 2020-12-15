32f91c3cedf87503344785809656b1a3437ed16e
xiao <xiao@xiaodeMacBook-Pro.local>
2020-12-15 23:11:55 +0800

    add *

--- model_v2/VGG.py
+++ model_v2/VGG.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import math
 
 cfg = {
     'VGG11': [64, 'M', 128, 'M', 256,'M', 512, 'M', 512,'M'],
@@ -8,30 +10,91 @@ cfg = {
     'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512],
 }
 
-class VGG(nn.Module):
-    def __init__(self, vgg_name):
-        super(VGG, self).__init__()
-        self.features = self._make_layers(cfg[vgg_name])
+class VGG19(nn.Module):
+    def __init__(self, init_weights=True):
+        super(VGG19, self).__init__()
+        features = self._make_layers(cfg['VGG19'], batch_norm=True)
+        model_list = list(features.children())
+        self.conv1_1 = model_list[0]
+        self.conv1_2 = model_list[2]
+        self.conv2_1 = model_list[5]
+        self.conv2_2 = model_list[7]
+        self.conv3_1 = model_list[10]
+        self.conv3_2 = model_list[12]
+        self.conv3_3 = model_list[14]
+        self.conv3_4 = model_list[16]
+        self.conv4_1 = model_list[19]
+        self.conv4_2 = model_list[21]
+        self.conv4_3 = model_list[23]
+        self.conv4_4 = model_list[25]
+        self.conv5_1 = model_list[28]
+        self.conv5_2 = model_list[30]
+        self.conv5_3 = model_list[32]
+        self.conv5_4 = model_list[34]
+        if init_weights:
+            self._initialize_weights()
 
     def forward(self, x):
-        out = self.features(x)
+        out = {}
+        out['conv1_1'] = F.relu(self.conv1_1(x))
+        out['conv1_2'] = F.relu(self.conv1_2(out['conv1_1']))
+        out['pool1'] = F.max_pool2d(out['conv1_2'])
+
+        out['conv2_1'] = F.relu(self.conv2_1(out['pool1']))
+        out['conv2_2'] = F.relu(self.conv2_2(out['conv2_1']))
+        out['pool2'] = F.max_pool2d(out['conv2_2'])
+
+        out['conv3_1'] = F.relu(self.conv3_1(out['pool2']))
+        out['conv3_2'] = F.relu(self.conv3_2(out['conv3_1']))
+        out['conv3_3'] = F.relu(self.conv3_3(out['conv3_2']))
+        out['conv3_4'] = F.relu(self.conv3_4(out['conv3_3']))
+        out['pool3'] = F.max_pool2d(out['conv3_4'])
+
+        out['conv4_1'] = F.relu(self.conv4_1(out['pool3']))
+        out['conv4_2'] = F.relu(self.conv4_2(out['conv4_1']))
+        out['conv4_3'] = F.relu(self.conv4_3(out['conv4_2']))
+        out['conv4_4'] = F.relu(self.conv4_4(out['conv4_3']))
+        out['pool4'] = F.max_pool2d(out['conv4_4'])
+
+        out['conv5_1'] = F.relu(self.conv5_1(out['pool4']))
+        out['conv5_2'] = F.relu(self.conv5_2(out['conv5_1']))
+        out['conv5_3'] = F.relu(self.conv5_3(out['conv5_2']))
+        out['conv5_4'] = F.relu(self.conv5_4(out['conv5_3']))
+        out['pool5'] = F.max_pool2d(out['conv5_4'])
+
+        # return [out[key] for key in out_key]
         return out
 
-    def _make_layers(self, cfg):
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+    def _make_layers(self, cfg, batch_norm=False):
         layers = []
         in_channels = 3
-        layers += [nn.Conv2d(3, 3, (1, 1))]
-        for x in cfg:
-            if x == 'M':
+        for v in cfg:
+            if v == 'M':
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
-                layers += [nn.ReflectionPad2d((1, 1, 1, 1)),
-                    nn.Conv2d(in_channels, x, kernel_size=3),
-                           nn.ReLU(inplace=True)]
-                in_channels = x
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                if batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.ReLU(inplace=True)]
+                in_channels = v
         return nn.Sequential(*layers)
 
 
 if __name__ == "__main__":
-    net = VGG('VGG19')
-    print(net.features)
+    net = VGG19()
+    print(net)
--- model_v2/model_v2.py
+++ model_v2/model_v2.py
@@ -10,71 +10,84 @@ from model_v2.VGG import *
 from model_v2.Decoder import *
 from model_v2.Transform import *
 
+def calc_mean_std(feat, eps=1e-5):
+    # eps is a small value added to the variance to avoid divide-by-zero.
+    size = feat.size()
+    assert (len(size) == 4)
+    N, C = size[:2]
+    feat_var = feat.view(N, C, -1).var(dim=2) + eps
+    feat_std = feat_var.sqrt().view(N, C, 1, 1)
+    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
+    return feat_mean, feat_std
 
-def conv_block(name, in_C, out_C, activation='ReLU', kernel_size=3, stride=1, padding=1, transpose=False):
-    block = nn.Sequential()
+def mean_variance_norm(feat):
+    size = feat.size()
+    mean, std = calc_mean_std(feat)
+    normalized_feat = (feat - mean.expand(size)) / std.expand(size)
+    return normalized_feat
 
-    if not transpose:
-        func = nn.Conv2d
-        conv = ' Conv'
-    else:
-        func = nn.ConvTranspose2d
-        conv = ' DeConv'
-    if activation == 'ReLU':
-        activate = nn.ReLU
-    elif activation == 'Tanh':
-        activate = nn.Tanh
+def _calc_feat_flatten_mean_std(feat):
+    # takes 3D feat (C, H, W), return mean and std of array within channels
+    assert (feat.size()[0] == 3)
+    assert (isinstance(feat, torch.FloatTensor))
+    feat_flatten = feat.view(3, -1)
+    mean = feat_flatten.mean(dim=-1, keepdim=True)
+    std = feat_flatten.std(dim=-1, keepdim=True)
+    return feat_flatten, mean, std
 
-    block.add_module(name + conv, func(in_C, out_C, kernel_size, stride, padding))
-    block.add_module(name + ' Inst_norm', nn.InstanceNorm2d(out_C))
-    if activation == 'ReLU':
-        block.add_module(name + ' ' + activation, activate(inplace=True))
-    elif activation == 'Tanh':
-        block.add_module(name + ' ' + activation, activate())
-
-    return block
 
 class NSTNet(nn.Module):
     def __init__(self, start_iter):
         super(NSTNet, self).__init__()
-        self.content_encoder = VGG('VGG19')
-        self.con_enc_1 = nn.Sequential(*list(self.content_encoder.children())[:4])  # input -> relu1_1
-        self.con_enc_2 = nn.Sequential(*list(self.content_encoder.children())[4:11])  # relu1_1 -> relu2_1
-        self.con_enc_3 = nn.Sequential(*list(self.content_encoder.children())[11:18])  # relu2_1 -> relu3_1
-        self.con_enc_4 = nn.Sequential(*list(self.content_encoder.children())[18:31])  # relu3_1 -> relu4_1
-        self.con_enc_5 = nn.Sequential(*list(self.content_encoder.children())[31:44])  # relu4_1 -> relu5_1
-
-        self.style_encoder = VGG('VGG19')
-        self.sty_enc_1 = nn.Sequential(*list(self.style_encoder.children())[:4])  # input -> relu1_1
-        self.sty_enc_2 = nn.Sequential(*list(self.style_encoder.children())[4:11])  # relu1_1 -> relu2_1
-        self.sty_enc_3 = nn.Sequential(*list(self.style_encoder.children())[11:18])  # relu2_1 -> relu3_1
-        self.sty_enc_4 = nn.Sequential(*list(self.style_encoder.children())[18:31])  # relu3_1 -> relu4_1
-        self.sty_enc_5 = nn.Sequential(*list(self.style_encoder.children())[31:44])  # relu4_1 -> relu5_1
-
+        self.content_encoder = VGG19
+        self.style_encoder = VGG19
         self.transform = Transform(in_planes=512)
         self.decoder = Decoder('Decoder_v2')
+
+        self.loss_net = VGG19
+        state_dict = torch.load('https://download.pytorch.org/models/vgg19_bn-c79401a0.pth')
+        state_dict = {k: v for k, v in state_dict.items() if 'class' not in k}
+        self.loss_net.load_state_dict(state_dict)
+
         if (start_iter > 0):
-            self.transform.load_state_dict(torch.load('/home/lwq/sdb1/xiaoxin/code/SANT_weight/transformer_iter_' + str(start_iter) + '.pth'))
-            self.decoder.load_state_dict(torch.load('/home/lwq/sdb1/xiaoxin/code/SANT_weight/decoder_iter_' + str(start_iter) + '.pth'))
+            self.transform.load_state_dict(
+                torch.load('/home/lwq/sdb1/xiaoxin/code/SANT_weight/transformer_iter_' + str(start_iter) + '.pth'))
+            self.decoder.load_state_dict(
+                torch.load('/home/lwq/sdb1/xiaoxin/code/SANT_weight/decoder_iter_' + str(start_iter) + '.pth'))
 
-    def extract_content_fea(self, content_img):
-        pass
+        self.mse_loss = nn.MSELoss()
 
-    def extract_style_fea(self, style_img):
-        pass
+
 
     def forward(self, content_img, style_img):
-        content_feas = self.extract_content_fea(content_img)
-        content_feas_s = self.extract_content_fea(style_img)
-        style_feas = self.extract_style_fea(style_img)
-
-        stylized_fea = self.transform(content_feas[3],content_feas_s[3], style_feas[3],
-                                      content_feas[4],content_feas_s[4], style_feas[4])
+        content_feas = self.content_encoder(content_img)
+        content_feas_s = self.content_encoder(style_img)
+        style_feas = self.style_encoder(style_img)
+        stylized_fea = self.transform(content_feas['conv4_1'], content_feas_s['conv4_1'], style_feas['conv4_1'],
+                                      content_feas['conv5_1'], content_feas_s['conv5_1'], style_feas['conv5_1'])
         stylized_img = self.decoder(stylized_fea)
-        return stylized_img
 
-    def compute_loss(self, content_img, style_img):
-        pass
+        #计算loss
+        loss = 0
+        return loss
+
+    def calc_content_loss(self, input, target, norm=False):
+        if (norm == False):
+            return self.mse_loss(input, target)
+        else:
+            return self.mse_loss(mean_variance_norm(input), mean_variance_norm(target))
+
+    def calc_style_loss(self, input, target):
+        input_mean, input_std = calc_mean_std(input)
+        target_mean, target_std = calc_mean_std(target)
+        return self.mse_loss(input_mean, target_mean) + \
+               self.mse_loss(input_std, target_std)
+
+    def calc_temporal_loss(self, x1, x2):
+        h = x1.shape[2]
+        w = x1.shape[3]
+        D = h * w
+        return self.mse_loss(x1, x2)
 
 
 if __name__ == '__main__':
