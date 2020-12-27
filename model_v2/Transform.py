e935e176f69020ce93b1fedbc3c7d6713c708434
lwq <396008241@qq.com>
2020-12-27 11:51:13 +0800

    model v2

--- model_v2/Transform.py
+++ model_v2/Transform.py
@@ -8,5 +8,4 @@ class Transform(nn.Module):
         self.merge_conv_pad = nn.ReflectionPad2d((1, 1, 1, 1))
         self.merge_conv = nn.Conv2d(in_planes*2, in_planes, (3, 3))
     def forward(self, content4_1, content4_1_s, style4_1, content5_1, content5_1_s, style5_1):
-        #print(content4_1.shape)
         return self.merge_conv(self.merge_conv_pad(self.sanet(content4_1, content4_1_s, style4_1) + self.upsample5_1(self.sanet(content5_1, content5_1_s, style5_1))))
--- test_v2.py
+++ test_v2.py
@@ -8,6 +8,7 @@ from os.path import basename
 from os.path import splitext
 from torchvision import transforms
 from torchvision.utils import save_image
+import glob
 
 def test_transform():
     transform_list = [transforms.Resize(size=(512, 512))]
@@ -18,17 +19,17 @@ def test_transform():
 parser = argparse.ArgumentParser()
 
 # Basic options
-parser.add_argument('--content', type=str, default='../content/chicago.png',
+parser.add_argument('--content', type=str, default='../content/*',
                     help='File path to the content image')
-parser.add_argument('--style', type=str, default='../style/27.jpg',
+parser.add_argument('--style', type=str, default='./style/*',
                     help='File path to the style image, or multiple style \
                     images separated by commas if you want to do style \
                     interpolation or spatial control')
 parser.add_argument('--steps', type=str, default=1)
-parser.add_argument('--content_encoder', type=str, default='../NST_v2/experiments/content_encoder_iter_50.pth')
-parser.add_argument('--style_encoder', type=str, default='../NST_v2/experiments/style_encoder_iter_50.pth')
-parser.add_argument('--decoder', type=str, default='../NST_v2/experiments/decoder_iter_50.pth')
-parser.add_argument('--transform', type=str, default='../NST_v2/experiments/transformer_iter_50.pth')
+parser.add_argument('--content_encoder', type=str, default='../NST_v2/experiments/content_encoder_iter_350000.pth')
+parser.add_argument('--style_encoder', type=str, default='../NST_v2/experiments/style_encoder_iter_350000.pth')
+parser.add_argument('--decoder', type=str, default='../NST_v2/experiments/decoder_iter_350000.pth')
+parser.add_argument('--transform', type=str, default='../NST_v2/experiments/transformer_iter_350000.pth')
 
 # Additional options
 parser.add_argument('--save_ext', default='.jpg',
@@ -39,7 +40,8 @@ parser.add_argument('--output', type=str, default='../output',
 # Advanced options
 args = parser.parse_args('')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
 
 if not os.path.exists(args.output):
     os.mkdir(args.output)
@@ -68,28 +70,42 @@ style_encoder.to(device)
 content_tf = test_transform()
 style_tf = test_transform()
 
-content = content_tf(Image.open(args.content))
-style = style_tf(Image.open(args.style))
+style_files = glob.glob(args.style)
+content_files = glob.glob(args.content)
+for content_file in content_files:
+    content = content_tf(Image.open(content_file))
+    content = content.to(device).unsqueeze(0)
+    for style_file in style_files:
+        # style = style_tf(Image.open(args.style))
+        print(content_file, style_file)
+        style_img = Image.open(style_file)
+        print(style_img.mode)
+        if style_img.mode != "RGB":
+            style_img = style_img.convert('RGB')
+        style = style_tf(style_img)
+        style = style.to(device).unsqueeze(0)
 
-style = style.to(device).unsqueeze(0)
-content = content.to(device).unsqueeze(0)
-with torch.no_grad():
-    for x in range(args.steps):
-        print('iteration ' + str(x))
+        with torch.no_grad():
+            for x in range(args.steps):
+                print('iteration ' + str(x))
 
-        content_feas = content_encoder(content)
-        content_feas_s = content_encoder(style)
-        style_feas = style_encoder(style)
-        stylized_fea = transform(content_feas['conv4_1'], content_feas_s['conv4_1'], style_feas['conv4_1'],
-                                      content_feas['conv5_1'], content_feas_s['conv5_1'], style_feas['conv5_1'])
-        stylized_img = decoder(stylized_fea)
+                content_feas = content_encoder(content)
+                content_feas_s = content_encoder(style)
+                style_feas = style_encoder(style)
+                stylized_fea = transform(content_feas['conv4_1'], content_feas_s['conv4_1'], style_feas['conv4_1'],
+                                              content_feas['conv5_1'], content_feas_s['conv5_1'], style_feas['conv5_1'])
+                stylized_img = decoder(stylized_fea)
 
-        stylized_img.clamp(0, 255)
+                stylized_img.clamp(0, 255)
 
-    stylized_img = stylized_img.cpu()
+            stylized_img = stylized_img.cpu()
 
-    output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(
-        args.output, splitext(basename(args.content))[0],
-        splitext(basename(args.style))[0], args.save_ext
-    )
-    save_image(stylized_img, output_name)
+            # output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(
+            #     args.output, splitext(basename(args.content))[0],
+            #     splitext(basename(args.style))[0], args.save_ext
+            # )
+            output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(
+                    args.output, splitext(basename(content_file))[0],
+                    splitext(basename(style_file))[0], args.save_ext
+                )
+            save_image(stylized_img, output_name)
